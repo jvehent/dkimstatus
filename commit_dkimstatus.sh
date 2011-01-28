f9add92339091d62e82d329eba8eb59be11f3712
Julien Vehent <julien@linuxwall.info>
2011-01-28 13:21:08 -0500

    updated german translation On branch master Changes to be committed: 	modified:   localization/de_DE.inc

--- commit_dkimstatus.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-tar -cvf dkimstatus-v`grep @version dkimstatus/dkimstatus.php | awk {'print $3'}`.tar dkimstatus --exclude-vcs
-scp dkimstatus-v`grep @version dkimstatus/dkimstatus.php | awk {'print $3'}`.tar julien@sachiel.linuxwall.info:/var/www/chroot-jve/var/www/ressources/code/
